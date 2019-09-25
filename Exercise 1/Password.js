6ce2c4990f2f3c9d40c82eedefda671650c3961a
Apurva Rai <apurvarai99@gmail.com>
2019-09-25 10:28:42 -0500

    Finished exercise 1

---
+++ Exercise 1/Password.js
@@ -0,0 +1,35 @@
+function authenticator()
+{
+
+var p1 = document.getElementById("p1").value;
+var p2 = document.getElementById("p2").value;
+
+  if(p1 == p2 && p1.length > 7)
+  {
+
+ alert("Validated! Passwords match and are at least 8 characters long.");
+
+  }
+
+  if(p1 !== p2 && p1.length > 7)
+  {
+
+    alert("Passwords do not match!");
+
+  }
+
+  else if(p1 == p2 && p1.length < 8)
+  {
+
+    alert("Passwords must be at least 8 characters long!");
+
+  }
+
+  else if(p1 !== p2 && (p1.length < 8 || p2.length < 8))
+  {
+
+    alert("Passwords do not match are not more than 8 characters long!")
+
+  }
+
+}
